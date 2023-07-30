76bd7039d0edecd8ce4393ff0e80c32fcb2195f3
ManuelLus7 <ManuelLus7@users.noreply.github.com>
2023-07-30 11:44:31 -0300

    Actualizando Carrito

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -3,42 +3,54 @@ import { CartContext } from '../context/CartContext';
 import { Link } from 'react-router-dom';
 
 const Carrito = () => {
-    const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext);
+  const { carrito, precioTotal, vaciarCarrito, aumentarCantidad, disminuirCantidad } = useContext(CartContext);
 
-    const handleVaciar = () => {
-        vaciarCarrito();
-    };
+  const handleVaciar = () => {
+    vaciarCarrito();
+  };
 
-    return (
-        <div className="container">
-            <h1 className="main-title">Carrito</h1>
+  const handleAumentarCantidad = (productId) => {
+    aumentarCantidad(productId);
+  };
 
-            {/* Mostrar los productos en el carrito */}
-            {carrito.length > 0 ? (
-                carrito.map((prod) => (
-                    <div key={prod.id}>
-                        <br />
-                        <h3>{prod.titulo}</h3>
-                        <p>Precio unit: ${prod.precio}</p>
-                        <p>Precio total: ${prod.precio * prod.cantidad}</p>
-                        <p>Cant: {prod.cantidad}</p>
-                        <br />
-                    </div>
-                ))
-            ) : (
-                <h2>El carrito se encuentra vacío</h2>
-            )}
+  const handleDisminuirCantidad = (productId) => {
+    disminuirCantidad(productId);
+  };
 
-            {/* Mostrar el precio total, botón de vaciar carrito y enlace a Finalizar compra */}
-            {carrito.length > 0 && (
-                <>
-                    <h2>Precio total: ${precioTotal()}</h2>
-                    <button onClick={handleVaciar}>Vaciar</button>
-                    <Link to="/checkout">Finalizar compra</Link>
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1 className="main-title">Carrito</h1>
+
+      {/* Mostrar los productos en el carrito */}
+      {carrito.length > 0 ? (
+        carrito.map((prod) => (
+          <div key={prod.id}>
+            <br />
+            <h3>{prod.titulo}</h3>
+            <p>Precio unit: ${prod.precio}</p>
+            <p>Precio total: ${prod.precio * prod.cantidad}</p>
+            <p>Cant: {prod.cantidad}</p>
+            <button onClick={() => handleAumentarCantidad(prod.id)}>+</button>
+            <button onClick={() => handleDisminuirCantidad(prod.id)}>-</button>
+            <br />
+          </div>
+        ))
+      ) : (
+        <h2>El carrito se encuentra vacío</h2>
+      )}
+
+      {/* Mostrar el precio total, botón de vaciar carrito y enlace a Finalizar compra */}
+      {carrito.length > 0 && (
+        <>
+          <h2>Precio total: ${precioTotal()}</h2>
+          <button onClick={handleVaciar}>Vaciar</button>
+          <Link to="/checkout">Finalizar compra</Link>
+          </br></br>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Carrito;
+
