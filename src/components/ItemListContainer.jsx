471c2a40e46b7ddc9ffc80e1581a62cc581c86ce
ManuelLus7 <ManuelLus7@users.noreply.github.com>
2023-07-29 23:41:49 -0300

    Actualizando Firebase

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,33 +5,43 @@ import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../firebase/config';
 
 const ItemListContainer = () => {
-    const [productos, setProductos] = useState([]);
-    const [titulo, setTitulo] = useState('Productos');
-    const { categoria } = useParams();
+  // Estado para almacenar la lista de productos obtenidos de Firebase
+  const [productos, setProductos] = useState([]);
 
-    useEffect(() => {
+  // Estado para almacenar el título de la lista (por defecto "Productos")
+  const [titulo, setTitulo] = useState('Productos');
 
-        const productosRef = collection(db, "productos");
-        const q = categoria ? query(productosRef, where("categoria", "==", categoria)) : productosRef;
-  
-        getDocs(q)
-          .then((resp) => {
-  
-            setProductos(
-              resp.docs.map((doc) => {
-                return { ...doc.data(), id: doc.id }
-              })
-            )
+  // Obtener el parámetro "categoria" de la URL utilizando useParams de react-router-dom
+  const { categoria } = useParams();
+
+  // UseEffect para realizar una consulta a Firestore y obtener los productos
+  useEffect(() => {
+    // Referencia a la colección "productos" en Firestore
+    const productosRef = collection(db, "productos");
+
+    // Si se especifica una categoría en la URL, agregar una cláusula "where" a la consulta
+    const q = categoria ? query(productosRef, where("categoria", "==", categoria)) : productosRef;
+
+    // Obtener los documentos (productos) de Firestore según la consulta
+    getDocs(q)
+      .then((resp) => {
+        // Almacenar los productos obtenidos en el estado "productos"
+        setProductos(
+          resp.docs.map((doc) => {
+            // Mapear cada documento a un objeto que incluye los datos y el ID del producto
+            return { ...doc.data(), id: doc.id };
           })
-          
-      }, [categoria])
-      
-      
-    return (
-      <div>
-          <ItemList productos={productos} titulo={titulo} />
-      </div>
-    )
-  }
-  
-  export default ItemListContainer
+        );
+      });
+  }, [categoria]); // Ejecutar el useEffect cuando cambia la categoría
+
+  return (
+    <div>
+      {/* Pasar la lista de productos y el título al componente ItemList */}
+      <ItemList productos={productos} titulo={titulo} />
+    </div>
+  );
+};
+
+export default ItemListContainer;
+
